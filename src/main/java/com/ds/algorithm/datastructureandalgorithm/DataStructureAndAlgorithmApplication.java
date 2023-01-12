718d4b0fe683c26ef4a30a04b513bce3bbe50376
OZLEMCITCI <ozlmctc@gmail.com>
2023-01-12 12:29:31 -0600

    initial push

---
+++ src/main/java/com/ds/algorithm/datastructureandalgorithm/DataStructureAndAlgorithmApplication.java
@@ -0,0 +1,12 @@
+package com.ds.algorithm.datastructureandalgorithm;
+
+
+
+//@SpringBootApplication
+public class DataStructureAndAlgorithmApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hello ");
+	}
+
+}
---
+++ src/test/java/com/ds/algorithm/datastructureandalgorithm/DatastructureAndAlgorithmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ds.algorithm.datastructureandalgorithm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatastructureAndAlgorithmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
