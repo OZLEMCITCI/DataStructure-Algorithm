a606f3e2d5194145e01e062eaa33185ea6e3943c
OZLEMCITCI <ozlmctc@gmail.com>
2023-01-14 21:03:14 -0600

    Unit test are added for dynamic array class

---
+++ src/main/java/com/ds/algorithm/datastructureandalgorithm/LinkedList.java
@@ -0,0 +1,43 @@
+package com.ds.algorithm.datastructureandalgorithm;
+
+public class LinkedList {
+
+    public class Node {
+    }
+
+    public Node head;
+
+    public void addFront(int data) {
+
+
+    }
+
+    public int getFirst() {
+
+    }
+
+    public int getLast() {
+
+    }
+
+    public void addBack(int data) {
+
+    }
+
+    public int size() {
+
+    }
+
+    public void clear() {
+
+    }
+
+    public void deleteValue(int data) {
+
+    }
+
+    public void print() {
+
+    }
+
+}
