6ed854804a957b07cbde7aa45003eea226fed654
OZLEMCITCI <ozlmctc@gmail.com>
2023-02-02 08:30:02 -0600

    LinkList functions are added

--- src/main/java/com/ds/algorithm/datastructureandalgorithm/datastructure/LinkedList.java
+++ src/main/java/com/ds/algorithm/datastructureandalgorithm/datastructure/LinkedList.java
@@ -1,43 +1,113 @@
 package com.ds.algorithm.datastructureandalgorithm.datastructure;
 
+import java.util.NoSuchElementException;
+
 public class LinkedList {
 
     public class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+        }
     }
+    int size=0;
 
     public Node head;
 
     public void addFront(int data) {
 
+        Node newNode=new Node(data);
+        if(head==null){
+           head=newNode;
+           size++;
+           return;
+        }
+        newNode.next=head;
+        head=newNode;
+        size++;
 
     }
 
     public int getFirst() {
-  return 0;
+        return head.data;
     }
 
     public int getLast() {
-return 0;
+        if(head==null){
+            throw new NoSuchElementException("Empty List");
+        }
+
+        Node traverser=head;
+
+        while(traverser.next!=null){
+            traverser=traverser.next;
+        }
+
+        return traverser.data;
+
     }
 
     public void addBack(int data) {
+        Node newNode=new Node(data);
+        if(head==null){
+            head=newNode;
+            size++;
+            return;
+        }
 
+        Node traverser=head;
+        while(traverser.next!=null){
+            traverser=traverser.next;
+        }
+
+        traverser.next=newNode;
+        size++;
     }
 
     public int size() {
-        return 0;
+        return size;
     }
 
     public void clear() {
-
+   head=null;
+   size=0;
     }
 
     public void deleteValue(int data) {
+        // if head
+        if (head == null) {
+            size--;
+            return;
+        }
+        if (head.data == data) {
+            head = head.next;
+            size--;
+            return;
+        }
 
+        // else walk the list
+        Node previous = head;
+
+        while (previous.next != null) {
+            if (previous.next.data == data) {
+                previous.next = previous.next.next;
+                size--;
+                return;
+            }
+            previous = previous.next;
+        }
+        size--;
     }
 
     public void print() {
-
+        Node current = head;
+        while (current != null) {
+            System.out.println(current.data);
+            current = current.next;
+        }
+        System.out.println("");
     }
 
 }
--- src/test/java/Test_LinkedList.java
+++ src/test/java/Test_LinkedList.java
@@ -1,2 +1,106 @@
+import org.junit.Assert;
+
+import com.ds.algorithm.datastructureandalgorithm.datastructure.LinkedList;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 public class Test_LinkedList {
+    private LinkedList linkedList;
+
+    @BeforeEach
+    public void SetUp() {
+        linkedList = new LinkedList();
+    }
+
+    @Test
+    public void AddFront() {
+        linkedList.addFront(1);
+        linkedList.addFront(2);
+        linkedList.addFront(3);
+
+        Assert.assertEquals(3, linkedList.getFirst());
+        Assert.assertEquals(1, linkedList.getLast());
+    }
+
+    @Test
+    public void GetFirst() {
+        linkedList.addFront(1);
+        Assert.assertEquals(1, linkedList.getFirst());
+    }
+
+    @Test
+    public void GetLast() {
+        linkedList.addFront(1);
+        linkedList.addFront(2);
+        linkedList.addFront(3);
+
+        Assert.assertEquals(1, linkedList.getLast());
+    }
+
+    @Test
+    public void AddBack() {
+        linkedList.addBack(1);
+        linkedList.addBack(2);
+        linkedList.addBack(3);
+
+        Assert.assertEquals(1, linkedList.getFirst());
+        Assert.assertEquals(3, linkedList.getLast());
+    }
+
+    @Test
+    public void Size() {
+        Assert.assertEquals(0, linkedList.size());
+        linkedList.addBack(1);
+        Assert.assertEquals(1, linkedList.size());
+        linkedList.addBack(2);
+        Assert.assertEquals(2, linkedList.size());
+    }
+
+    @Test
+    public void Clear() {
+        linkedList.addBack(1);
+        linkedList.addBack(2);
+        linkedList.addBack(3);
+
+        linkedList.clear();
+
+        Assert.assertEquals(0, linkedList.size());
+    }
+
+    @Test
+    public void DeleteValue() {
+        linkedList.addBack(1);
+        linkedList.addBack(2);
+        linkedList.addBack(3);
+
+        linkedList.deleteValue(2);
+
+        Assert.assertEquals(2, linkedList.size());
+        Assert.assertEquals(1, linkedList.getFirst());
+        Assert.assertEquals(3, linkedList.getLast());
+    }
+
+    @Test
+    public void DeleteLastValue() {
+        linkedList.addBack(1);
+        linkedList.addBack(2);
+        linkedList.addBack(3);
+        linkedList.addBack(4);
+        //linkedList.clear();
+        System.out.println(linkedList.size());
+
+        linkedList.deleteValue(5
+        );
+
+//        Assert.assertEquals(3, linkedList.size());
+//        Assert.assertEquals(1, linkedList.getFirst());
+//        Assert.assertEquals(3, linkedList.getLast());
+
+        linkedList.print();
+    }
+
 }
